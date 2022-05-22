5da5d87591d4eb145ae2e7bc502b749c41d502ad
Muhammad Rafif Elfazri <rafif.elfazri@gmail.com>
2022-05-22 16:04:29 +0700

    update

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,9 +3,7 @@ require('winston-logstash');
 
 logger.add(logger.transports.Logstash, {
   port: 13299,
-  host:
-    process.env.LOGIT_IO_ENDPOINT ||
-    '2f357c18-6f29-4201-91e1-2883e9a45764-ls.logit.io',
+  host: '2f357c18-6f29-4201-91e1-2883e9a45764-ls.logit.io',
   ssl_enable: true,
   max_connect_retries: -1,
 });
